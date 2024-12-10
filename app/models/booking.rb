57690756bca7611cab838de833d8f59bf2876487
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2024-12-10 01:42:47 +0100

    user journey, reste plus qu'à ajouter un moyen de paiement pour le back

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :package
-  belongs_to :client
 
   enum status: { pending: 0, paid: 1, failed: 2 }
 
   validates :address, presence: true
   validates :date, presence: true
   validates :time, presence: true
-  validates :status, presence: true
 end
