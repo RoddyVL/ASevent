853052844f9a28756f51a0a5a5098212d590eaea
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-07 16:18:49 +0100

    migration

--- db/migrate/20241216212558_drop_p_booking.rb
+++
@@ -1,5 +0,0 @@
-class DropPBooking < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :p_bookings, if_exists: true, force: :cascade
-  end
-end
--- db/migrate/20241216212640_drop_p_package.rb
+++
@@ -1,5 +0,0 @@
-class DropPPackage < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :p_orders
-  end
-end
