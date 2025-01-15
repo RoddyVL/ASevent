bb80870cd99059fd226fde1dca05f00bec821bf6
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-15 04:06:30 +0100

    bhkb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def set_browser
+    @browser = Browser.new(request.user_agent)
+    @is_mobile = @browser.device.mobile?
+  end
+  
   def set_stripe_key
     @stripe_public_key = ENV['STRIPE_PUBLIC_KEY']
   end
