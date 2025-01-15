2f62b76421a61b0c1e710242c39fcacb6b128174
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-15 04:00:52 +0100

    nhkkj

--- db/migrate/20241216213040_add_price_to_packages.rb
+++ db/migrate/20241216213040_add_price_to_packages.rb
@@ -1,5 +1,8 @@
 class AddPriceToPackages < ActiveRecord::Migration[7.1]
   def change
-    add_monetize :packages, :price, currency: { present: false }
+    # Vérifier si la colonne n'existe pas déjà avant de l'ajouter
+    unless column_exists?(:packages, :price_cents)
+      add_monetize :packages, :price, currency: { present: false }
+    end
   end
 end
