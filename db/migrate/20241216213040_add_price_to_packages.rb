3c4f00a8c26406550142b4004250e0a2a02cee23
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-15 03:38:05 +0100

    jhbnjlk

--- db/migrate/20241216213040_add_price_to_packages.rb
+++ db/migrate/20241216213040_add_price_to_packages.rb
@@ -1,4 +1,4 @@
-class AddPriceToPackage < ActiveRecord::Migration[7:1]
+class AddPriceToPackage < ActiveRecord::Migration[7.1]
   def change
     add_monetize :packages, :price, currency: {present: false}
   end
