58626c6df1108bb94eb2cb67296b14dfe082f11c
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-17 03:25:08 +0100

    icon panier added

--- app/javascript/controllers/chats_list_controller.js
+++ app/javascript/controllers/chats_list_controller.js
@@ -2,12 +2,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
- static targets = ["chat"]
+ static targets = ["chat", "chatContainer"]
 
   highlight(event) {
     this.chatTargets.forEach((chat) => {
       chat.classList.remove("active")
     });
     event.currentTarget.classList.add("active");
+  }
+
+  toggleChat() {
+    if (this.hasChatContainerTarget) {
+      const container = this.chatContainerTarget;
+      const icon = 
+      container.classList.toggle("d-none");
+    }
   }
 }
