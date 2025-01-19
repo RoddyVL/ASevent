1ab0ca0d12fcf588475608be56784fcf0dd0b020
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-01-19 22:28:27 +0100

    solid queu and open AI API key setup

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module ASevent
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :solid_queue
   end
 end
